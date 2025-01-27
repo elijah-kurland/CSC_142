8b93c6419ca16131ac2326afb223744312a3b3d8
Elijah-Kurland <captainrex017@gmail.com>
2025-01-27 11:23:17 -0500

    Add files via upload

---
+++ Examples-and-Assignment1/car-ride.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){
+    std::string desc;
+    std::cout << "Please enter a description of your car.";
+    std::getline(std::cin, desc);
+    std::cout << "Great Ride!" << std::endl;
+}
---
+++ Examples-and-Assignment1/if.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int max;
+    max = 5;
+
+    if (max == 5)
+        std::cout << "max is 5" << std::endl;
+
+    return 0;
+}
